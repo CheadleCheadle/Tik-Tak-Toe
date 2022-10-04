38958437a52a0d1ea017fd68123bc52801f34252
CheadleCheadle <gcheadle123@gmail.com>
2022-10-03 20:41:38 -0700

    first commit

--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -10,35 +10,67 @@ class Cursor {
     this.col = 0;
 
     this.gridColor = 'black';
-    this.cursorColor = 'yellow';
-
+    this.cursorColor = 'red';
+   
   }
 
   resetBackgroundColor() {
     Screen.setBackgroundColor(this.row, this.col, this.gridColor);
-  }
+  } 
 
   setBackgroundColor() {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
   }
 
   up() {
-    // Move cursor up
+    
+
+    if (this.row > 0){
+      this.resetBackgroundColor();
+      --this.row;
+      this.setBackgroundColor();
+      Screen.render();
+      Screen.printCommands();
+    }
+    
   }
 
   down() {
-    // Move cursor down
+    if (this.row < this.numRows - 1) {
+    this.resetBackgroundColor();
+     ++this.row;
+    this.setBackgroundColor();
+     Screen.render();
+     Screen.printCommands();
+    }
+  
   }
 
   left() {
-    // Move cursor left
+    if (this.col > 0) {
+    this.resetBackgroundColor();
+    --this.col;
+    this.setBackgroundColor();
+    Screen.render();
+    Screen.printCommands();
+    }
+ 
   }
 
   right() {
-    // Move cursor right
+    if (this.col < this.numCols - 1) {
+    this.resetBackgroundColor();
+    ++this.col;
+    this.setBackgroundColor();
+    Screen.render();
+    Screen.printCommands();
+    } 
+  
   }
 
 }
+
+
 
 
 module.exports = Cursor;
--- starter/class/ttt.js
+++ starter/class/ttt.js
@@ -16,27 +16,87 @@ class TTT {
     // Initialize a 3x3 tic-tac-toe grid
     Screen.initialize(3, 3);
     Screen.setGridlines(true);
+    
+    // Screen.setBackgroundColor(this.cursor.row, this.cursor.col, 'black');
+    // Screen.setTextColor(this.cursor.row, this.cursor.col, 'red');
 
+    
     // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', TTT.testCommand);
 
+  
+    Screen.addCommand('d', 'moves cursor right', this.cursor.right.bind(this.cursor));
+    
+    Screen.addCommand('a', 'moves cursor left', this.cursor.left.bind(this.cursor));
+    
+    Screen.addCommand('w', 'moves cursor up', this.cursor.up.bind(this.cursor));
+    
+    Screen.addCommand('s', 'moves cursor down', this.cursor.down.bind(this.cursor));
+   
+    Screen.addCommand('space','place move', TTT.turn.bind(this));
+    
     Screen.render();
+    Screen.printCommands();
   }
 
-  // Remove this
-  static testCommand() {
-    console.log("TEST COMMAND");
-  }
+  
+static turn() {
+  Screen.render();
+  Screen.setGrid(this.cursor.row, this.cursor.col, this.playerTurn);
+  
+  this.playerTurn == "O" ? this.playerTurn = "X" : this.playerTurn = "O";
 
+  const winner = TTT.checkWin(Screen.grid);
+
+ 
+  if (!winner) {
+    Screen.render();
+  } else {
+    TTT.endGame(winner);
+  }
+}
+
+
+  static rotateGrid(grid) {
+    let newGrid = [];
+    for (const col in grid) {
+      let rotated = grid.map(row => row[col]);
+      newGrid.push(rotated);
+    }
+    return newGrid;
+  }
   static checkWin(grid) {
+   const copy = TTT.rotateGrid(grid);
 
-    // Return 'X' if player X wins
-    // Return 'O' if player O wins
-    // Return 'T' if the game is a tie
-    // Return false if the game has not ended
+   let choices = ["X","O"];
 
+   for (const player in choices) {
+    for (const row in grid) {
+      if (grid[row].every(el => el == choices[player])) {
+        return choices[player];
+      }
+    }
+   
+
+   for (const col in copy) {
+    if (copy[col].every(el => el == choices[player])) {
+      return choices[player];
+    }
+   }
+    if((grid[0][0] == choices[player]) &&
+    (grid[0][0] == grid[1][1]) &&
+    (grid[1][1] == grid[2][2]))
+    return choices[player];
+   else if (
+    (grid[0][2] == choices[player]) &&
+    (grid[0][0] == grid[1][1]) &&
+    (grid[1][1] == grid[2][0])
+   )  return choices[player];
   }
-
+  if (grid.every(row => row.every(el => el != ' '))) return "T";
+  
+    else return false;
+  
+  }
   static endGame(winner) {
     if (winner === 'O' || winner === 'X') {
       Screen.setMessage(`Player ${winner} wins!`);
@@ -50,5 +110,12 @@ class TTT {
   }
 
 }
+ 
+
+const grid =[['X','X','X'],
+[' ',' ',' '],
+[' ',' ',' ']];
+console.log(TTT.checkWin(grid));
+
 
 module.exports = TTT;
